22254d0d73e9d62cf7a5cc2a64dbcc0e08b68da4
Rameshwari Kodam <rameshwarik2@gmail.com>
2022-05-04 14:31:02 +0530

    Pattern programs

--- Patterns/simpleStarPattern.cpp
+++ Patterns/simpleStarPattern.cpp
@@ -59,3 +59,44 @@ int main() {
 
     return 0;
 }
+
+
+// n=4
+//    *
+//   **
+//  ***
+// ****
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here;
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row <= n) {
+        int space = n-row;
+        while(space){
+            cout<<"  ";
+            space--;
+        }
+        int col = 1;
+        while(col<=row){
+            cout<<"*";
+            col++;
+        }
+        cout<<"\n";
+        row++;
+    }
+    return 0;
+}
+
+// ****
+// ***
+// **
+// *
+
+
+// ****
+//  ***
+//   **
+//    *
