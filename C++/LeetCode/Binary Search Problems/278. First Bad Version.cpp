37eb7351d14eab3a02ed4771fbe8dae823c08765
Rameshwari Kodam <rameshwarik2@gmail.com>
2022-07-02 16:30:15 +0530

    refactor

--- C++/LeetCode/Binary Search Problems/278. First Bad Version.cpp
+++ C++/LeetCode/Binary Search Problems/278. First Bad Version.cpp
@@ -1,6 +1,5 @@
 /*
-
-*/You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
 Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 
@@ -26,6 +25,7 @@ Output: 1
 Constraints:
 
 1 <= bad <= n <= 231 - 1
+*/
 
 class Solution {
 public:
@@ -35,12 +35,10 @@ public:
             int mid=s+(n-s)/2;
             if(isBadVersion(mid)){
                 ans=mid;
-                s=mid+1;
-                // n=mid-1;
+                n=mid-1;
             }
             else{
-                n=mid-1;
-                // s=mid+1;
+                s=mid+1;
             }
         }
         return ans;
